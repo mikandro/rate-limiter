4a993e8d8352dc0ed6e74c460ae1850f00b5477c
mikandro <mihail.andritchi@gmail.com>
2024-10-30 07:00:22 +0100

    Added public interface and the consturctor method

--- ratelimit.go
+++ ratelimit.go
@@ -1,14 +1,24 @@
 package ratelimit
 
 import (
+	"sync"
 	"time"
 )
 
-type Limiter interface {
-	Take() time.Time
+type RateLimiter struct {
+	capacity      int
+	tokens        int
+	rate          time.Duration
+	lastRefreshed time.Time
+	mutex         sync.Mutex
 }
 
-type Clock interface {
-	Now() time.Time
-	Sleep(time.Duration)
+func NewRateLimiter(capacity, rate) *RateLimiter {
+	return &RateLimiter{
+		capacity: capacity,
+		tokens: capacity,
+		rate: rate,
+		lastRefreshed: time.Now(),
+		mutex: sync.Mutex{}
+	}
 }
